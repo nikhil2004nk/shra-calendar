744009d9d7487e81ff6a04bccad8f95e8cb3bb85
nikhil2004nk <nikhil2004nk@gmail.com>
2025-12-18 00:04:25 +0530

    Update workflow to deploy from nik-branch

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/shra-calendar/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
